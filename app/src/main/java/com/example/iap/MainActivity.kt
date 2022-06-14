19f70d43a1f92cb20184fad1e5feda6c0056b48a
Aliakbar Moradhasel <aliakbarmoradhaseli@gmail.com>
2022-06-14 14:11:01 +0430

    add api chack purchase

--- app/src/main/java/com/example/iap/MainActivity.kt
+++ app/src/main/java/com/example/iap/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.iap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.hupad_iap.HupadIAP
+import com.example.iap.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var iap: HupadIAP = HupadIAP(this, { productId: String, error: String ->
+        Toast.makeText(this, error, Toast.LENGTH_LONG).show()
+    }) { productId: String, trackingCode: String? ->
+        Toast.makeText(this, "Payment was successful \ntxid: $trackingCode", Toast.LENGTH_LONG)
+            .show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.purchase.setOnClickListener { view ->
+            iap.iapOnClick("f70a7225-e413-4138-9431-104958489623")
+        }
+
     }
 }
--- hupad-iap/src/main/java/com/example/hupad_iap/HupadIAP.kt
+++ hupad-iap/src/main/java/com/example/hupad_iap/HupadIAP.kt
@@ -1,24 +1,70 @@
 package com.example.hupad_iap
 
+import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.ComponentName
 import android.content.Intent
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 class HupadIAP(
     private val activity: AppCompatActivity,
-    private val onCLick: (s: String, b: Boolean) -> Unit
+    private val onError: (productId: String, error: String) -> Unit,
+    private val onSuccess: (productId: String, trackingCode: String) -> Unit
 ) {
     private val resultLauncher =
         activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == 521378) {
+            if (result.resultCode == Activity.RESULT_OK) {
                 val data: Intent? = result.data
-                onCLick.invoke(
-                    data?.getStringExtra("productId") ?: "",
-                    data?.getBooleanExtra("result", false)!!
-                )
+                val trackingCode = data?.getStringExtra("trackingCode")
+                if (trackingCode != null) {
+
+                    OkHttpClient().newCall(
+                        Request.Builder()
+                            .url("https://master.hupadstore.ir/api/Wallet/CheckPurchase")
+                            .method(
+                                "POST", RequestBody.create(
+                                    MediaType.parse("application/json"),
+                                    "{\"txid\":\"$trackingCode\"}"
+                                )
+                            )
+                            .addHeader("Content-Type", "application/json")
+                            .build()
+                    ).enqueue(object : Callback {
+                        override fun onFailure(call: Call, e: IOException) {
+                            onError.invoke(
+                                data.getStringExtra("productId") ?: "",
+                                e.toString()
+                            )
+                        }
+
+                        override fun onResponse(call: Call, response: Response) {
+                            val jsonData = response.body()!!.string()
+                            val jObject = JSONObject(jsonData)
+                            val status = jObject.getInt("status")
+                            if (status == 1)
+                                onSuccess.invoke(
+                                    data.getStringExtra("productId")!!,
+                                    jObject.getString("entity")
+                                )
+                            else onError.invoke(
+                                data.getStringExtra("productId") ?: "",
+                                JSONObject(
+                                    data.getStringExtra("error") ?: ""
+                                ).getString(data.getStringExtra("message") ?: "") ?: ""
+                            )
+                        }
+                    })
+                } else {
+                    onError.invoke(
+                        data?.getStringExtra("productId") ?: "",
+                        "field"
+                    )
+                }
             }
         }
 
